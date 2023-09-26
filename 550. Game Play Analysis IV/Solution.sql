e9771f49df4d8abea2bccf11463662ead1181ab3
Bharti Kumari <75694208+bhartik021@users.noreply.github.com>
2023-09-26 20:51:40 +0530

    Create Solution.sql

---
+++ 550. Game Play Analysis IV/Solution.sql
@@ -0,0 +1,7 @@
+SELECT ROUND(SUM(login)/COUNT(DISTINCT player_id), 2) AS fraction
+FROM (
+  SELECT
+    player_id,
+    DATEDIFF(event_date, MIN(event_date) OVER(PARTITION BY player_id)) = 1 AS login
+  FROM Activity
+) AS t
