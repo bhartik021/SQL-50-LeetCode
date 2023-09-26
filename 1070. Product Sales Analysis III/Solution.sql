991d37bd0c89a1126ff46f241fee11b7f87f8aeb
Bharti Kumari <75694208+bhartik021@users.noreply.github.com>
2023-09-26 21:01:54 +0530

    Create Solution.sql

---
+++ 1070. Product Sales Analysis III/Solution.sql
@@ -0,0 +1,8 @@
+# Write your MySQL query statement below
+SELECT product_id, year AS first_year, quantity, price
+FROM Sales
+WHERE (product_id, year) in (
+    SELECT product_id, MIN(year) 
+    FROM Sales
+    GROUP BY product_id
+)
